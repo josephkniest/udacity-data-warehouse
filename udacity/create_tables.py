740e71b51cf3fbc1c7f90efd36f11d8157088420
Joe <joe@yis-macbook-pro.lan>
2020-11-19 13:15:36 -0800

    Load each file from s3 into memory

--- udacity/create_tables.py
+++ udacity/create_tables.py
@@ -2,12 +2,17 @@ from boto3.session import Session
 import boto3
 import json
 
-def main():
+def load_s3():
     file = open('./creds.json', "r")
     creds = json.loads(file.read())
     file.close()
-    s3 = boto3.client('s3', aws_access_key_id=creds['keyId'], aws_secret_access_key=creds['keySec'])
-    print(s3.list_objects(Bucket='scpro2-udacity-data-engineering'))
+    return boto3.resource('s3', aws_access_key_id=creds['keyId'], aws_secret_access_key=creds['keySec'])
+
+def main():
+    s3 = load_s3()
+    bucket = s3.Bucket('scpro2-udacity-data-engineering')
+    for obj in bucket.objects.all():
+        print(obj.key, '->', obj.get()['Body'].read())
 
 if __name__ == "__main__":
     main()
